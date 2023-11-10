9baaac50298c9ae961ab314a086b876e55197f96
BtcAutoNode <149917291+BtcAutoNode@users.noreply.github.com>
2023-11-10 23:47:47 +0100

    Add files via upload
    add install scripts

--- install/0_install_system.sh
+++ install/0_install_system.sh
@@ -1,1 +1,477 @@
+#!/bin/bash
+
+#
+### prepare system / install dependencies / create user etc...
+#
+
+#-----------------------------------------------------------------
+
+#
+### Config
+#
+source CONFIG
+
+#-----------------------------------------------------------------
+
+#
+### check if root, otherwise exit
+#
+if [ "$EUID" -ne 0 ]; then
+  echo -e "${R}Please run the installation script as root!${NC}"
+  exit
+fi
+
+#-----------------------------------------------------------------
+
+# clear screen
+clear
+
+#-----------------------------------------------------------------
+
+#
+### Check if user really wants to install...or exit
+#
+echo
+echo -e "${Y}This script will update and prepare the system for the node installation...${NC}"
+echo
+echo -e "${LB}The following steps will be executed in the process:${NC}"
+echo "- Upgrade system to newest software (apt update / apt upgrade)"
+echo "- Install missing dependencies via apt. The following packages are going to be installed:"
+echo -e "  - ${LP}${INSTALL_PKGS}${NC}"
+echo "- Install newest NodeJS and NPM (version ${NODEJS_VERSION}+)"
+echo "- Configure correct timezone (by user interaction)"
+echo "- Create user ${USER} as the working user for the apps"
+echo "- Set password for user ${USER} (by user interaction)"
+echo "- Change .bashrc file for user ${USER} (aliases, prompt,...)"
+echo "- Create .ssh dir for ${USER} in ${HOME_DIR}/ and create .ssh/authorized_keys file"
+echo "- Allow ssh login via public key and restart of ssh daemon"
+echo "- Create .ssh dir for root in /root/ and create .ssh/authorized_keys file"
+echo "- Change .bashrc file for root (aliases, prompt,...)"
+echo "- Allow ${USER} to execute systemctl command without entering pw"
+echo "- Create Nginx config ${NGINX_CONF_FILE}"
+echo "- Delete Nginx default site in /etc/nginx/sites-enabled/default"
+echo "- Create SSL certificates for nginx"
+echo "- Check configs and restart Nginx"
+echo "- Install tor from tor repositories (for newest version)"
+echo "- Add user ${USER} to tor group"
+echo "- Configure tor in ${TORRC_CONF_FILE} file and restart tor"
+echo
+echo -e "${LR}Press ${NC}<enter>${LR} key to continue, or ${NC}ctrl-c${LR} to exit${NC}"
+read -r
+
+#-----------------------------------------------------------------
+
+# clear screen
+clear
+
+#-----------------------------------------------------------------
+
+#
+### update / upgrade system
+#
+echo
+echo -e "${Y}Updating the system via apt...${NC}"
+apt-get -q update && apt upgrade -y
+echo -e "${G}Done.${NC}"
+
+#-----------------------------------------------------------------
+
+#
+### install dependencies
+#
+echo
+echo -e "${Y}Installing dependencies...${NC}"
+for i in ${INSTALL_PKGS}; do
+  echo -e "${LB}Installing package ${i} ...${NC}"
+  apt-get -q install -y "${i}"
+  echo -e "${LB}Done.${NC}"
+done
+echo -e "${G}Done.${NC}"
+
+#-----------------------------------------------------------------
+
+#
+### install newest nodejs/npm version
+### NodeJS/npm installation: https://github.com/nodesource/distributions#installation-instructions
+#
+echo
+echo -e "${Y}Installing NodeJS/npm from their repository...${NC}"
+echo -e "${LB}Creating /etc/apt/keyrings...${NC}"
+mkdir -p ${KEYRINGS_DIR}
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o "${NODEJS_KEYRING_FILE}"
+NODE_MAJOR="${NODEJS_VERSION}"
+echo -e "${LB}Add nodejs repository to ${NODEJS_LIST_FILE}...${NC}"
+echo "deb [signed-by=${NODEJS_KEYRING_FILE}] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee "${NODEJS_LIST_FILE}"
+echo -e "${LB}Update apt and install...${NC}"
+apt-get -q update
+apt-get -q --reinstall install nodejs -y
+echo -e "${G}Done.${NC}"
+
+#-----------------------------------------------------------------
+
+#
+### configure the right timezone (by user interaction)
+#
+echo
+echo -e "${Y}Configuring timezone...${NC}"
+echo -e "${LR}Please select the right area and city for your location in the opening application. Press ${NC}<enter>${LR} key to continue...${NC}"
+read -r
+dpkg-reconfigure tzdata
+echo -e "${G}Done.${NC}"
+
+#-----------------------------------------------------------------
+
+#
+### quietly add user satoshi without password
+#
+echo
+echo -e "${Y}Create user ${USER}...${NC}"
+id -u "${USER}" &>/dev/null || adduser --quiet --disabled-password --shell /bin/bash --home /home/"${USER}" --gecos "User" "${USER}"
+echo -e "${G}Done.${NC}"
+
+#-----------------------------------------------------------------
+
+#
+### set password for the user (by user input)
+#
+echo
+echo -e "${Y}Set a password for user ${USER}...${NC}"
+echo -e "${LR}Please enter a password for the user ${NC}${USER}${LR} and press the ${NC}<enter>${LR} key...${NC}"
+read -r USER_PASSWORD
+# set/change
+echo "satoshi:${USER_PASSWORD}" | chpasswd
+echo -e "${G}Done.${NC}"
+
+#-----------------------------------------------------------------
+
+#
+### request user to note down user password
+#
+echo
+echo -e "${LR}Note down the user password just entered!! Press ${NC}<enter>${LR} key to go on:${NC}"
+read -r
+
+#-----------------------------------------------------------------
+
+#
+### change .bashrc file for user satoshi (append stuff at the end)
+#
+echo
+echo -e "${Y}Add content to .bashrc file for user ${USER}...${NC}"
+cat >> "${HOME_DIR}/.bashrc"<< EOF
+
+
+# list alias
+alias l='ls -alF --color'
+
+# Home dir
+set home dir
+if [ -z "${HOME:-}" ]; then
+  export HOME="$(cd ~ && pwd)";
+fi
+
+#Prompt (user):
+PS1='\[\033[01;36m\]\u\[\033[01;33m\]@\[\033[01;36m\]\h \[\033[01;33m\][\w] \[\033[01;35m\]\$ \[\033[00m\]'
+
+# Hostname Message
+toilet -f smblock -f future --filter border:gay '${HOSTNAME}'
+cd ~
+EOF
+echo -e "${G}Done.${NC}"
+
+#-----------------------------------------------------------------
+
+#
+### create .ssh dir for satoshi and create authorized_keys file
+#
+echo
+echo -e "${Y}Creating .ssh dir for ${USER} and create authorized_keys file...${NC}"
+# Create the .ssh directory
+echo -e "${LB}Creating ${HOME_DIR}/.ssh dir...${NC}"
+mkdir -p "${HOME_DIR}"/.ssh
+echo -e "${LB}Creating authorized_keys file in ${HOME_DIR}/.ssh...${NC}"
+# Create the authorized keys file
+touch "${HOME_DIR}"/.ssh/authorized_keys
+echo -e "${G}Done.${NC}"
+
+#-----------------------------------------------------------------
+
+#
+### allow user satoshi to execute systemctl command without entering pw
+#
+echo
+echo -e "${Y}Allow user ${USER} to use systemctl cmd without password...${NC}"
+echo "${USER} ALL=(ALL) NOPASSWD: /usr/bin/systemctl" | EDITOR='tee -a' visudo
+echo -e "${G}Done.${NC}"
+
+#-----------------------------------------------------------------
+
+#
+### create download dir for user satoshi
+#
+echo
+echo -e "${Y}Create downloads directory for user satoshi...${NC}"
+if [ ! -d "${DOWNLOAD_DIR}" ]; then
+  mkdir -p "${DOWNLOAD_DIR}"
+fi
+echo -e "${G}Done.${NC}"
+
+#-----------------------------------------------------------------
+
+#
+### change permissions of files/directories for user satoshi
+#
+echo
+echo -e "${Y}Change permissions of files/directories for users ${USER}...${NC}"
+# download dir
+chown -R "${USER}":"${USER}" "${DOWNLOAD_DIR}"
+# .ssh dir
+chown -R "${USER}":"${USER}" "${HOME_DIR}"/.ssh
+chmod 0700 "${HOME_DIR}"/.ssh
+chown "${USER}":"${USER}" "${HOME_DIR}"/.ssh/authorized_keys
+chmod 0600 "${HOME_DIR}"/.ssh/authorized_keys
+echo -e "${G}Done.${NC}"
+
+#-----------------------------------------------------------------
+
+#
+### allow ssh login with public key in /etc/ssh/sshd_config
+### change config line with sed and restart sshd service
+#
+echo
+echo -e "${Y}Allow ssh login via public key in /etc/ssh/sshd_config...${NC}"
+sed -i "s/^#PubkeyAuthentication.*/PubkeyAuthentication yes/g" /etc/ssh/sshd_config
+# restart sshd service
+echo -e "${LB}Restarting ssh daemon...${NC}"
+systemctl restart sshd
+echo -e "${G}Done.${NC}"
+
+#-----------------------------------------------------------------
+
+#
+### create .ssh dir for root and create authorized_keys file
+#
+echo
+echo -e "${Y}Creating .ssh dir for root user and create authorized_keys file...${NC}"
+# Create the .ssh directory, and set its permissions
+echo -e "${LB}Creating /root/.ssh dir and change permissions...${NC}"
+mkdir -p /root/.ssh
+chmod 0700 /root/.ssh
+echo -e "${LB}Creating authorized_keys file in /root/.ssh and change permissions...${NC}"
+# Create the authorized keys file, and set its permissions
+touch /root/.ssh/authorized_keys
+chmod 0600 /root/.ssh/authorized_keys
+echo -e "${G}Done.${NC}"
+
+#-----------------------------------------------------------------
+
+#
+### change .bashrc file for root (append stuff at the end)
+#
+echo
+echo -e "${Y}Add content to .bashrc file for root...${NC}"
+cat > "/root/.bashrc"<< EOF
+# list alias
+alias l='ls -alF --color'
+alias upd='apt update && apt upgrade -y'
+
+# Home dir
+set home dir
+if [ -z "${HOME:-}" ]; then
+  export HOME="$(cd ~ && pwd)";
+fi
+
+# Prompt(root):
+PS1='\[\033[01;31m\]\u\[\033[01;33m\]@\[\033[01;36m\]\h \[\033[01;33m\][\w] \[\033[01;35m\]\$ \[\033[00m\]'
+
+# Hostname Message
+toilet -f smblock -f future --filter border:gay '${HOSTNAME}'
+cd ~
+EOF
+echo -e "${LB}Enter: 'source .bashrc' (to reload after script is finished)${NC}"
+echo -e "${G}Done.${NC}"
+
+#-----------------------------------------------------------------
+
+#
+### nginx main config
+#
+echo
+echo -e "${Y}Write Nginx main config file...${NC}"
+mv "${NGINX_CONF_FILE}" "${NGINX_CONF_FILE}.bak"
+cat > "${NGINX_CONF_FILE}"<< EOF
+# nginx main conf
+
+user www-data;
+worker_processes auto;
+pid /run/nginx.pid;
+include /etc/nginx/modules-enabled/*.conf;
+
+events {
+        worker_connections 768;
+}
+
+http {
+        sendfile on;
+        tcp_nopush on;
+        tcp_nodelay on;
+        keepalive_timeout 65;
+        types_hash_max_size 2048;
+        server_tokens off;
+        include /etc/nginx/mime.types;
+        default_type application/octet-stream;
+        ssl_protocols TLSv1.3;
+        ssl_prefer_server_ciphers on;
+        access_log /var/log/nginx/access.log;
+        error_log /var/log/nginx/error.log;
+        gzip on;
+        include /etc/nginx/conf.d/*.conf;
+        include /etc/nginx/sites-enabled/*;
+}
+
+stream {
+        ssl_certificate /etc/ssl/certs/nginx-selfsigned.crt;
+        ssl_certificate_key /etc/ssl/private/nginx-selfsigned.key;
+        ssl_session_cache shared:SSL:1m;
+        ssl_session_timeout 4h;
+        ssl_protocols TLSv1.3;
+        ssl_prefer_server_ciphers on;
+        include /etc/nginx/streams-enabled/*.conf;
+}
+EOF
+echo -e "${G}Done.${NC}"
+
+#-----------------------------------------------------------------
+
+#
+### delete nginx default site in site-enabled
+#
+echo
+echo -e "${Y}Delete nginx default site in site-enabled...${NC}"
+rm -f /etc/nginx/sites-enabled/default
+echo -e "${G}Done.${NC}"
+
+#-----------------------------------------------------------------
+
+#
+### create ssl certs for nginx
+#
+echo
+echo -e "${Y}Create ssl certificates for use in Nginx...${NC}"
+openssl req -x509 -nodes -newkey rsa:4096 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/CN=localhost" -days 3650
+echo -e "${G}Done.${NC}"
+
+#-----------------------------------------------------------------
+
+#
+### check nginx config
+#
+echo
+echo -e "${Y}Checking nginx configs...${NC}"
+nginx -t
+if [ "$?" -eq 0 ]; then
+  echo -e "${G}Nginx configs: OK${NC}"
+else
+  echo -e "${R}Nginx configs: Not OK${NC}"
+  exit
+fi
+echo -e "${G}Done.${NC}"
+
+#-----------------------------------------------------------------
+
+#
+### restart nginx webserver
+#
+echo
+echo -e "${Y}Restart Nginx web server...${NC}"
+systemctl restart nginx
+echo -e "${G}Done.${NC}"
+
+#-----------------------------------------------------------------
+
+#
+### Install tor from tor repository (to always have newest version)
+#
+echo
+echo -e "${Y}Install tor from tor repositories (for newest version)...${NC}"
+echo -e "${LB}Add tor repository to ${TOR_LIST_FILE}...${NC}"
+cat > "${TOR_LIST_FILE}"<< EOF
+deb     [arch=amd64 signed-by=${TOR_KEYRING_FILE}] https://deb.torproject.org/torproject.org ${RELEASE_CODE} main
+deb-src [arch=amd64 signed-by=${TOR_KEYRING_FILE}] https://deb.torproject.org/torproject.org ${RELEASE_CODE} main
+EOF
+# get gpg key
+wget -qO- https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | gpg --dearmor | tee "${TOR_KEYRING_FILE}" >/dev/null
+# install tor and tor debian keyring
+echo -e "${LB}Update apt and install...${NC}"
+apt -q update
+apt -q install tor deb.torproject.org-keyring -y
+echo -e "${G}Done.${NC}"
+
+#-----------------------------------------------------------------
+
+#
+### Add user satoshi to tor group
+#
+echo
+echo -e "${Y}Add user ${USER} to tor group...${NC}"
+usermod -a -G debian-tor "${USER}"
+echo -e "${G}Done.${NC}"
+
+#-----------------------------------------------------------------
+
+#
+### Tor
+#
+# configure tor
+echo
+echo -e "${Y}Edit /etc/tor/torrc config...${NC}"
+# append tor config at end of torrc config file
+#{ echo "ControlPort 9051"; \
+#  echo "CookieAuthentication 1"; \
+#  echo "CookieAuthFileGroupReadable 1"; \
+#  echo "SocksPort localhost:9050";} >> "${TORRC_CONF_FILE}"
+grep -qxF 'ControlPort 9051' "${TORRC_CONF_FILE}" || echo 'ControlPort 9051' >> "${TORRC_CONF_FILE}"
+grep -qxF 'CookieAuthentication 1' "${TORRC_CONF_FILE}" || echo 'CookieAuthentication 1' >> "${TORRC_CONF_FILE}"
+grep -qxF 'CookieAuthFileGroupReadable 1' "${TORRC_CONF_FILE}" || echo 'CookieAuthFileGroupReadable 1' >> "${TORRC_CONF_FILE}"
+grep -qxF 'SocksPort localhost:9050' "${TORRC_CONF_FILE}" || echo 'SocksPort localhost:9050' >> "${TORRC_CONF_FILE}"
+# restart tor
+echo -e "${Y}Restarting tor service...${NC}"
+systemctl restart tor
+echo -e "${G}Done.${NC}"
+
+#-----------------------------------------------------------------
+
+echo
+echo -e "${Y}Installation all done!${NC}"
+echo
+echo -e "${LB}User for the applications:${NC}"
+echo -e " User: + ${USER}\n" \
+        "User home dir: + ${HOME_DIR}\n" \
+        "User download dir: + ${DOWNLOAD_DIR}\n" \
+        "+" | column -t -s "+"
+echo
+echo -e " ${LB}Files and Directories:${NC}"
+echo -e " ${TORRC_CONF_FILE} + - Tor torrc config file\n" \
+        "${TORSOCKS_CONF_FILE} + - Tor socks proxy config file\n" \
+        "${TOR_LIST_FILE} + - file for Tor deb archive links\n" \
+        "${TOR_KEYRING_FILE} + - Tor gpg keyring file\n" \
+        "+\n" \
+        "${NODEJS_LIST_FILE} + - file for Nodejs deb archive links\n" \
+        "${NODEJS_KEYRING_FILE} + - Nodejs gpg keyring file\n" \
+        "+\n" \
+        "${NGINX_CONF_FILE} + - Nginx web server config file\n" \
+        "${NGINX_WEBROOT_DIR} + - Nginx web root dir\n" \
+        "+\n" \
+        "${HOME_DIR}/.ssh + - ssh dir for user ${USER} (authorized_keys file)\n" \
+        "/root/.ssh + - ssh dir for user root (authorized_keys file)" | column -t -s "+"
+echo
+echo -e "${LB}Enter 'source .bashrc' to reload the changed root profile.${NC}"
+echo -e "${LB}You can then use 'l' for dir listings in color'.${NC}"
+echo
+echo -e "${LB}IP address of the machine: ${LOCAL_IP}${NC}"
+echo
+echo -e "${LB}For security reasons you should connect to the server via ssh keys.${NC}"
+echo -e "${LB}See the chapter 'Login with SSH keys' for instructions:${NC}"
+echo " https://raspibolt.org/guide/raspberry-pi/security.html#login-with-ssh-keys"
+echo
 
---
+++ install/6_install_sparrow.sh
@@ -0,0 +1,252 @@
+#!/bin/bash
+
+#
+### downdload, verify, install Sparrow Wallet terminal/server
+#
+
+#-----------------------------------------------------------------
+
+#
+### Config
+#
+source CONFIG
+
+#-----------------------------------------------------------------
+
+#
+### check if root, otherwise exit
+#
+if [ "$EUID" -ne 0 ]; then
+  echo -e "${R}Please run the installation script as root!${NC}"
+  exit
+fi
+
+#-----------------------------------------------------------------
+
+# clear screen
+clear
+
+#-----------------------------------------------------------------
+
+#
+### Check if user really wants to install...or exit
+#
+echo
+echo -e "${Y}This script will install Sparrow Wallet terminal/server...${NC}"
+echo
+echo -e "${LB}The following steps will be executed in the process:${NC}"
+echo "- Download of the release from sparrowwallet.com/download/"
+echo "- Verify the release file"
+echo "- Extract release and move extracted folder to /opt/Sparrow"
+echo "- Create symbolic link in /usr/local/bin to Sparrow application in /opt"
+echo "- Create .sparrow base dir in ${USER} home dir (${HOME_DIR})"
+echo "- Edit config file in .sparrow to connect to fulcrum ssl"
+echo "- Change permission of Sparrow download and base dir for ${USER}"
+echo
+echo -e "${LR}Press ${NC}<enter>${LR} key to continue, or ${NC}ctrl-c${LR} to exit${NC}"
+read -r
+
+#-----------------------------------------------------------------
+
+# clear screen
+clear
+
+#-----------------------------------------------------------------
+
+#
+### update / upgrade system
+#
+echo
+echo -e "${Y}Updating the system via apt...${NC}"
+apt-get -q update && apt upgrade -y
+echo -e "${G}Done.${NC}"
+
+#-----------------------------------------------------------------
+
+#
+### create download dir
+#
+echo
+echo -e "${Y}Create downloads directory for Sparrow for user ${USER}...${NC}"
+mkdir -p "${SPARROW_DOWNLOAD_DIR}"
+echo -e "${G}Done.${NC}"
+
+#-----------------------------------------------------------------
+
+#
+### download (and overwrite) what is needed for sparrow terminal
+#
+echo
+echo -e "${Y}Download Sparrow terminal release files...${NC}"
+cd "${SPARROW_DOWNLOAD_DIR}"
+# sparrow terminal release
+wget -O sparrow-server-"${SPARROW_VERSION}"-x86_64.tar.gz \
+        https://github.com/sparrowwallet/sparrow/releases/download/"${SPARROW_VERSION}"/sparrow-server-"${SPARROW_VERSION}"-x86_64.tar.gz
+wget -O sparrow-"${SPARROW_VERSION}"-manifest.txt \
+        https://github.com/sparrowwallet/sparrow/releases/download/"${SPARROW_VERSION}"/sparrow-"${SPARROW_VERSION}"-manifest.txt
+wget -O sparrow-"${SPARROW_VERSION}"-manifest.txt.asc \
+        https://github.com/sparrowwallet/sparrow/releases/download/"${SPARROW_VERSION}"/sparrow-"${SPARROW_VERSION}"-manifest.txt.asc
+echo -e "${G}Done.${NC}"
+
+
+#-----------------------------------------------------------------
+
+#
+### verify the release
+#
+echo
+echo -e "${Y}Verify the release files...${NC}"
+# checksum
+sha256sum --ignore-missing --check --status sparrow-"${SPARROW_VERSION}"-manifest.txt 
+if [ "$?" -eq 0 ]; then
+  echo -e "${G}Verification of release checksum in checksum file: OK${NC}"
+else
+  echo -e "${R}Verification of release checksum: Not OK${NC}"
+  exit
+fi
+# download some gpg keys
+wget -O pgp_keys.asc https://keybase.io/craigraw/pgp_keys.asc
+# import into gpg
+gpg --import -q pgp_keys.asc
+gpg --verify sparrow-"${SPARROW_VERSION}"-manifest.txt.asc 2>&1 >/dev/null | grep 'Good Signature'
+if [ "$?" = 0 ]; then
+  echo -e "${R}The signature(s) for the downloaded file are not good signature. Exiting now.${NC}"
+  exit 1
+else
+  echo -e "${G}The signature(s) for the downloaded file are good signature(s).${NC}"
+fi
+echo -e "${G}Done.${NC}"
+
+#-----------------------------------------------------------------
+
+#
+### extract and move aplication folder into /opt/Sparrow and link to /usr/local/bin
+#
+echo
+echo -e "${Y}Extract release, move to /opt and install the sparrow app into /usr/local/bin/...${NC}"
+cd "${SPARROW_DOWNLOAD_DIR}"
+echo -e "${LB}Extract release file${NC}"
+tar xfz sparrow-server-"${SPARROW_VERSION}"-x86_64.tar.gz
+# move folder to /opt
+echo -e "${LB}Move extracted folder to /opt${NC}"
+# delete /opt/Sparrow if it does exist
+rm -rf /opt/Sparrow
+mv "${SPARROW_DOWNLOAD_DIR}/Sparrow" /opt
+# create symbolic link in /usr/local/bin
+echo -e "${LB}Create symbolic link in /usr/local/bin to Sparrow app in /opt${NC}"
+ln -sf "${SPARROW_APP_DIR}"/bin/Sparrow "${SPARROW_SYM_LINK}"
+echo -e "${G}Done.${NC}"
+
+#-----------------------------------------------------------------
+
+#
+### create sparrow base dir structure in the users home dir
+#
+echo
+echo -e "${Y}Create sparrow base dir structure in ${USER} home dir${NC}"
+echo -e "${LB}Create .sparrow folder in user home dir${NC}"
+mkdir -p "${SPARROW_DIR}"
+echo -e "${LB}Create config and log files${NC}"
+touch "${SPARROW_CONF_FILE}"
+touch "${SPARROW_LOG_FILE}"
+echo -e "${LB}Create directories${NC}"
+mkdir -p "${SPARROW_DIR}/certs"
+mkdir -p "${SPARROW_DIR}/wallets"
+mkdir -p "${SPARROW_DIR}/wallets/backup"
+echo -e "${G}Done.${NC}"
+
+#-----------------------------------------------------------------
+
+#
+### create config and write content to the config
+#
+echo
+echo -e "${Y}Create and configure sparrow terminal config file...${NC}"
+cat > "${SPARROW_CONF_FILE}"<< EOF
+{
+  "mode": "ONLINE",
+  "bitcoinUnit": "AUTO",
+  "unitFormat": "COMMA",
+  "fiatCurrency": "EUR",
+  "exchangeSource": "COINGECKO",
+  "loadRecentWallets": true,
+  "validateDerivationPaths": true,
+  "groupByAddress": true,
+  "includeMempoolOutputs": true,
+  "notifyNewTransactions": true,
+  "checkNewVersions": true,
+  "openWalletsInNewWindows": false,
+  "hideEmptyUsedAddresses": false,
+  "showTransactionHex": true,
+  "showLoadingLog": true,
+  "showAddressTransactionCount": false,
+  "showDeprecatedImportExport": false,
+  "signBsmsExports": false,
+  "preventSleep": false,
+  "recentWalletFiles": [
+  ],
+  "keyDerivationPeriod": 4305,
+  "dustAttackThreshold": 1000,
+  "enumerateHwPeriod": 30,
+  "serverType": "ELECTRUM_SERVER",
+  "useLegacyCoreWallet": false,
+  "electrumServer": "ssl://127.0.0.1:50002|fulcrum ssl",
+  "recentElectrumServers": [
+  ],
+  "useProxy": true,
+  "proxyServer": "127.0.0.1:9050",
+  "autoSwitchProxy": true,
+  "maxServerTimeout": 34,
+  "maxPageSize": 100,
+  "usePayNym": false,
+  "sameAppMixing": false
+}
+EOF
+echo -e "${G}Done.${NC}"
+
+#-----------------------------------------------------------------
+
+#
+### chown downloads and sparrow base dir for user
+#
+echo
+echo -e "${Y}Chown downloads and sparrow base dir for users permissions...${NC}"
+chown -R "${USER}":"${USER}" "${SPARROW_DOWNLOAD_DIR}"
+chown -R "${USER}":"${USER}" "${SPARROW_DIR}"
+echo -e "${G}Done.${NC}"
+
+#-----------------------------------------------------------------
+
+echo
+echo -e "${Y}Installation all done!${NC}"
+echo
+echo -e "${LB}User for the applications:${NC}"
+echo -e " Sparrow dir: + - ${SPARROW_DIR} (config/wallets/log)\n" \
+        "Sparrow download dir: + - ${SPARROW_DOWNLOAD_DIR}" | column -t -s "+"
+echo
+echo -e "${LB}Files and Directories:${NC}"
+echo -e " ${SPARROW_CONF_FILE} + - Sparrow config file\n" \
+        "${SPARROW_LOG_FILE} + - Sparrow log file\n" \
+        "+\n" \
+        "${SPARROW_APP_DIR} + - Sparrow installation dir\n" \
+        "${SPARROW_SYM_LINK} + - Sparrow user executable link" | column -t -s "+"
+echo
+echo
+echo -e "${LB}If you have an existing hot wallet for mixing, copy the ${NC}*.mv.db${LB} file into the ${NC}.sparrow/wallets${LB} folder.${NC}"
+echo
+echo -e "${LB}Start a tmux session (to keep app running after logout): ${NC}tmux new -s sparrow_server${NC}"
+echo -e "${LB}Then execute the Sparrow app: ${NC}Sparrow${NC}"
+echo -e "${LB}Check that you are connected to your node in the Preferences menu.${NC}"
+echo
+echo -e "${LB}Check how mixing works in the SparrowWallet docs:${NC}"
+echo " https://www.sparrowwallet.com/docs/mixing-whirlpool.html"
+echo
+echo -e "${LB}To leave and let the Sparrow app open in background for continous mixing...${NC}"
+echo -e "${LB} lock your wallet(s) via menu, then press ${NC}<ctrl-b>${LB}, release fingers and then press ${NC}<d>${LB}.${NC}"
+echo -e "${LB}The next time you want to access the running session, type: ${NC}tmux a${LB} (and later leave the same way).${NC}"
+echo
+echo -e "${LB}If you want to close Sparrow, enter the tmux session and select ${NC}Quit${LB} from the Sparrow menu.${NC}"
+echo -e "${LB}Then ${NC}exit${LB} to close the tmux session.${NC}"
+echo
+
+
