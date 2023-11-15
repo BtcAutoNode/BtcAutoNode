829bc4bd77034328d4a1901b69b3a3f9198eda99
BtcAutoNode <149917291+BtcAutoNode@users.noreply.github.com>
2023-11-15 23:19:29 +0100

    Update upgrade_2_fulcrum.sh

--- upgrade/upgrade_2_fulcrum.sh
+++ upgrade/upgrade_2_fulcrum.sh
@@ -4,6 +4,9 @@
 ### upgrade fulcrum (if possible)
 #
 
+# fail if a command fails and exit
+set -e
+
 #-----------------------------------------------------------------
 
 #
@@ -140,10 +143,10 @@ fi
 # download gpg key
 wget -O calinkey.txt https://raw.githubusercontent.com/Electron-Cash/keys-n-hashes/master/pubkeys/calinkey.txt
 # import into gpg
-gpg --import -q calinkey.txt
+gpg --import -q calinkey.txt || true
 # verify
-gpg --verify Fulcrum-"${FULCRUM_VERSION}"-sha256sums.txt.asc 2>&1 >/dev/null | grep 'Good Signature'
-if [ "$?" = 0 ]; then
+gpg --verify Fulcrum-"${FULCRUM_VERSION}"-sha256sums.txt.asc 2>&1 >/dev/null | grep 'Good signature'
+if [ "$?" != 0 ]; then
   echo -e "${R}The signature(s) for the downloaded file are not good signature. Exiting now.${NC}"
   exit 1
 else
