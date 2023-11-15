7af081087e2e144a203c882a511194e6226cf3da
BtcAutoNode <149917291+BtcAutoNode@users.noreply.github.com>
2023-11-15 23:23:12 +0100

    Update upgrade_2_fulcrum.sh

--- upgrade/upgrade_2_fulcrum.sh
+++ upgrade/upgrade_2_fulcrum.sh
@@ -32,6 +32,9 @@ fi
 if [ "$EUID" -ne 0 ]; then
   echo -e "${R}Please run the installation script as root!${NC}"
   exit
+else
+  # set PATH env var for sbin and bin dirs (su root fails the installation)
+  export PATH=/usr/local/sbin:/usr/sbin:/sbin:/usr/local/bin:/usr/bin:/bin
 fi
 
 #-----------------------------------------------------------------
