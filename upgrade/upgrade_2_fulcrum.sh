0fa773122e06216ab96023e67bc30f7094604e32
BtcAutoNode <149917291+BtcAutoNode@users.noreply.github.com>
2023-11-15 23:39:41 +0100

    Update upgrade_2_fulcrum.sh (#77)

--- upgrade/upgrade_2_fulcrum.sh
+++ upgrade/upgrade_2_fulcrum.sh
@@ -70,8 +70,8 @@ echo
 echo -e "${Y}Check Github version and compare to running version...${NC}"
 latest_version=$(curl -sL https://github.com/cculianu/Fulcrum/releases/latest | grep "<title>Release" | cut -d ' ' -f 5)
 running_version=$("${FULCRUM_DIR}"/Fulcrum --version | grep Release | cut -d ' ' -f 2)
-echo "Latest version on Github : $latest_version"
-echo "Current version running  : $running_version"
+echo "Latest version on Github : ${latest_version}"
+echo "Current version running  : ${running_version}"
 echo
 if [ "$latest_version" = "$running_version" ]; then
   echo -e "${R}No new version available...exiting${NC}"
@@ -81,7 +81,7 @@ else
   echo -e "${G}New version ${latest_version} available...possible to upgrade${NC}"
 fi
 # replace so existing commands can be used
-FULCRUM_VERSION="$latest_version"
+FULCRUM_VERSION="{$latest_version}"
 #echo -e "${G}Done.${NC}"
 
 #-----------------------------------------------------------------
