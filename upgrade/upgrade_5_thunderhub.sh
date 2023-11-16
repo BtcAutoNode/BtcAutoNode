d32702ae8acc5574b244ebbc113c4f2906645e3c
BtcAutoNode <149917291+BtcAutoNode@users.noreply.github.com>
2023-11-16 01:12:25 +0100

    Update upgrade_5_thunderhub.sh

--- upgrade/upgrade_5_thunderhub.sh
+++ upgrade/upgrade_5_thunderhub.sh
@@ -122,10 +122,9 @@ echo -e "${LB}This can take several minutes!${NC}"
 # update
 git config --global --add safe.directory "${THH_DIR}"
 cd "${THH_DIR}"
-#git fetch
-#git checkout "v${latest_version}"
 # update npm
 npm install -g npm@10.2.4
+# update thunderhub
 npm run update
 echo -e "${G}Done.${NC}"
 
