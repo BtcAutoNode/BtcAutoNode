a9cbb1d570754cbb15b783e132bf589533653e57
BtcAutoNode <149917291+BtcAutoNode@users.noreply.github.com>
2024-03-07 00:13:55 +0100

    Update uninstall_3_mempool.sh
    upd mempool uninstall script
    - added process steps
    - fixed typos and other smaller issues

--- uninstall/uninstall_3_mempool.sh
+++ uninstall/uninstall_3_mempool.sh
@@ -27,11 +27,37 @@ fi
 
 #-----------------------------------------------------------------
 
+# clear screen
+clear
+
+#-----------------------------------------------------------------
+
+#
+### Check if user really wants to uninstall...or exit
+#
+echo
+echo -e "${Y}This script will uninstall all files/folders of the Mempool installation...${NC}"
+echo
+echo -e "${LB}The following steps will be executed in the process:${NC}"
+echo "- Stop mempool systemd service (${MEMPOOL_SERVICE})"
+echo "- Disable mempool systemd service (${MEMPOOL_SERVICE})"
+echo "- Delete mempool systemd service file (${MEMPOOL_SERVICE_FILE})"
+echo "- Delete mempool base dir (${MEMPOOL_DIR})"
+echo "- Delete mempool maria-db database (db mempool)"
+echo "- Delete mempool nginx config files"
+echo "- Delete mempool from nginx webroot dir (${MEMPOOL_WEBROOT_DIR})"
+echo "- Restart nginx web server"
+echo
+echo -e "${LR}Press ${NC}<enter>${LR} key to continue, or ${NC}ctrl-c${LR} to exit${NC}"
+read -r
+
+#-----------------------------------------------------------------
+
 #
 # stop mempool service
 #
 echo
-echo -e "${Y}Stop mempool service...${NC}"
+echo -e "${Y}Stop Mempool service (${MEMPOOL_SERVICE})...${NC}"
 systemctl stop "${MEMPOOL_SERVICE}"
 echo -e "${G}Done.${NC}"
 
@@ -41,7 +67,7 @@ echo -e "${G}Done.${NC}"
 # disable mempool service
 #
 echo
-echo -e "${Y}Disable mempool service...${NC}"
+echo -e "${Y}Disable Mempool service (${MEMPOOL_SERVICE})...${NC}"
 systemctl disable "${MEMPOOL_SERVICE}"
 echo -e "${G}Done.${NC}"
 
@@ -51,8 +77,8 @@ echo -e "${G}Done.${NC}"
 # delete mempool service file
 #
 echo
-echo -e "${Y}Deleting Mempool service file (${MEMPOOL_SERVICE_FILE})...${NC}"
-rm "${MEMPOOL_SERVICE_FILE}"
+echo -e "${Y}Delete Mempool service file (${MEMPOOL_SERVICE_FILE})...${NC}"
+rm -f "${MEMPOOL_SERVICE_FILE}"
 echo -e "${G}Done.${NC}"
 
 #-----------------------------------------------------------------
@@ -61,29 +87,17 @@ echo -e "${G}Done.${NC}"
 # delete mempool git download dir
 #
 echo
-echo -e "${Y}Deleting Mempool dir in ${HOME_DIR}...${NC}"
+echo -e "${Y}Delete Mempool dir (${MEMPOOL_DIR})...${NC}"
 rm -rf "${MEMPOOL_DIR}"
 echo -e "${G}Done.${NC}"
 
 #-----------------------------------------------------------------
 
 #
-# delete npm cache dir and .cache/Cypress dir)
+# delete mempool maria-db/mysql database
 #
 echo
-echo -e "${Y}Deleting npm cache dir...${NC}"
-npm cache clean --force
-rm -rf $(npm get cache)
-rm -rf /root/.cache/Cypress
-echo -e "${G}Done.${NC}"
-
-#-----------------------------------------------------------------
-
-#
-# delete mempool maria-db database
-#
-echo
-echo -e "${Y}Deleting Mempool maria-db databse (mempool)...${NC}"
+echo -e "${Y}Delete Mempool maria-db/mysql database (db mempool)...${NC}"
 mysql -e "drop database mempool;"
 echo -e "${G}Done.${NC}"
 
@@ -93,10 +107,10 @@ echo -e "${G}Done.${NC}"
 # nginx config files
 #
 echo
-echo -e "${Y}Deleting Mempool nginx files...${NC}"
-rm "${MEMPOOL_NGINX_SSL_CONF}"
-rm "${MEMPOOL_NGINX_SNIPPET}"
-rm /etc/nginx/sites-enabled/mempool-ssl.conf
+echo -e "${Y}Delete Mempool nginx files...${NC}"
+rm -f "${MEMPOOL_NGINX_SSL_CONF}"
+rm -f "${MEMPOOL_NGINX_SNIPPET}"
+rm -f /etc/nginx/sites-enabled/mempool-ssl.conf
 echo -e "${G}Done.${NC}"
 
 #-----------------------------------------------------------------
@@ -105,7 +119,7 @@ echo -e "${G}Done.${NC}"
 # delete mempool from nginx webroot dir
 #
 echo
-echo -e "${Y}Deleting mempool webroot folder in /var/www/html/...${NC}"
+echo -e "${Y}Delete Mempool webroot folder in /var/www/html/...${NC}"
 rm -rf "${MEMPOOL_WEBROOT_DIR}"
 echo -e "${G}Done.${NC}"
 
@@ -122,6 +136,5 @@ echo -e "${G}Done.${NC}"
 #-----------------------------------------------------------------
 
 echo
-echo -e "${Y}Un-installation all done!${NC}"
+echo -e "${Y}Uninstallation all done!${NC}"
 echo
-
