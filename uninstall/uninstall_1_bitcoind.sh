952f01030379a7fd4a4bd750aaf27bd27024e00a
BtcAutoNode <149917291+BtcAutoNode@users.noreply.github.com>
2024-03-07 00:12:36 +0100

    Update uninstall_1_bitcoind.sh
    upd bitcoin uninstall script
    - added process steps
    - fixed typos and other smaller issues

--- uninstall/uninstall_1_bitcoind.sh
+++ uninstall/uninstall_1_bitcoind.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 #
 ### check if CONFIG file is there and not empty, otherwise exit
 #
@@ -28,22 +27,56 @@ fi
 
 #-----------------------------------------------------------------
 
+# clear screen
+clear
+
+#-----------------------------------------------------------------
+
 #
-# stop bitcoin service
+### Check if user really wants to uninstall...or exit
 #
 echo
-echo -e "${Y}Stop bitcoind service...${NC}"
-systemctl stop bitcoind.service
+echo -e "${Y}This script will uninstall all files/folders of the Bitcoin installation...${NC}"
+echo
+echo -e "${LB}The following steps will be executed in the process:${NC}"
+echo "- Stop bitcoind systemd service (${BITCOIN_SERVICE})"
+echo "- Disable bitcoind systemd service (${BITCOIN_SERVICE})"
+echo "- Delete bitcoind systemd service file (${BITCOIN_SERVICE_FILE})"
+echo "- Delete bitcoin download dir (${BITCOIN_DOWNLOAD_DIR})"
+echo "- Delete bitcoin base dir (${BITCOIN_DIR})"
+echo "- Delete bitcoin app binaries from /usr/local/bin"
+echo
+echo -e "${LR}Press ${NC}<enter>${LR} key to continue, or ${NC}ctrl-c${LR} to exit${NC}"
+read -r
+
+#-----------------------------------------------------------------
+
+#
+# stop bitcoind service
+#
+echo
+echo -e "${Y}Stop bitcoind service (${BITCOIN_SERVICE})...${NC}"
+systemctl stop "${BITCOIN_SERVICE}"
 echo -e "${G}Done.${NC}"
 
 #-----------------------------------------------------------------
 
 #
-# disable bitcoin service
+# disable bitcoind service
 #
 echo
-echo -e "${Y}Disable bitcoind service...${NC}"
-systemctl disable bitcoind.service
+echo -e "${Y}Disable bitcoind service (${BITCOIN_SERVICE})...${NC}"
+systemctl disable "${BITCOIN_SERVICE}"
+echo -e "${G}Done.${NC}"
+
+#-----------------------------------------------------------------
+
+#
+# delete bitcoind service file
+#
+echo
+echo -e "${Y}Delete bitcoind systemd service file (${BITCOIN_SERVICE_FILE})...${NC}"
+rm -f "${BITCOIN_SERVICE_FILE}"
 echo -e "${G}Done.${NC}"
 
 #-----------------------------------------------------------------
@@ -52,28 +85,18 @@ echo -e "${G}Done.${NC}"
 # delete bitcoin download dir
 #
 echo
-echo -e "${Y}Delete bitcoin download dir...${NC}"
+echo -e "${Y}Delete bitcoin download dir (${BITCOIN_DOWNLOAD_DIR})...${NC}"
 rm -rf "${BITCOIN_DOWNLOAD_DIR}"
 echo -e "${G}Done.${NC}"
 
 #-----------------------------------------------------------------
 
 #
-# delete .bitcoin dir
+# delete .bitcoin base dir
 #
 echo
-echo -e "${Y}Delete bitcoin base dir (.bitcoin)...${NC}"
+echo -e "${Y}Delete bitcoin base dir (${BITCOIN_DIR})...${NC}"
 rm -rf "${BITCOIN_DIR}"
-echo -e "${G}Done.${NC}"
-
-#-----------------------------------------------------------------
-
-#
-# delete bitcoin service file
-#
-echo
-echo -e "${Y}Delete bitcoin system service sile...${NC}"
-rm "${BITCOIN_SERVICE_FILE}"
 echo -e "${G}Done.${NC}"
 
 #-----------------------------------------------------------------
@@ -83,17 +106,17 @@ echo -e "${G}Done.${NC}"
 #
 echo
 echo -e "${Y}Delete Bitcoin apps from /usr/local/bin...${NC}"
-rm "/usr/local/bin/bitcoin-cli"
-rm "/usr/local/bin/bitcoin-qt"
-rm "/usr/local/bin/bitcoin-tx"
-rm "/usr/local/bin/bitcoin-util"
-rm "/usr/local/bin/bitcoin-wallet"
-rm "/usr/local/bin/bitcoind"
-rm "/usr/local/bin/test_bitcoin"
+rm -f "/usr/local/bin/bitcoin-cli"
+rm -f "/usr/local/bin/bitcoin-qt"
+rm -f "/usr/local/bin/bitcoin-tx"
+rm -f "/usr/local/bin/bitcoin-util"
+rm -f "/usr/local/bin/bitcoin-wallet"
+rm -f "/usr/local/bin/bitcoind"
+rm -f "/usr/local/bin/test_bitcoin"
 echo -e "${G}Done.${NC}"
 
 #-----------------------------------------------------------------
 
 echo
-echo -e "${Y}Un-installation all done!${NC}"
+echo -e "${Y}Uninstallation all done!${NC}"
 echo
