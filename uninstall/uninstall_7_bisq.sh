6ca2a6b9bf39f8edc113a3677bef1ee016b12044
BtcAutoNode <149917291+BtcAutoNode@users.noreply.github.com>
2024-03-07 00:15:42 +0100

    Update uninstall_7_bisq.sh
    upd bisq uninstall script
    - added process steps
    - fixed typos and other smaller issues

--- uninstall/uninstall_7_bisq.sh
+++ uninstall/uninstall_7_bisq.sh
@@ -27,12 +27,39 @@ fi
 
 #-----------------------------------------------------------------
 
+# clear screen
+clear
+
+#-----------------------------------------------------------------
+
+#
+### Check if user really wants to uninstall...or exit
+#
+echo
+echo -e "${Y}This script will uninstall all files/folders of the Bisq installation...${NC}"
+echo
+echo -e "${LB}The following steps will be executed in the process:${NC}"
+echo "- Stop bisq xpra server via stop script"
+echo "- Stop bisq gradle server(s)"
+echo "- Delete bisq download dir (${BISQ_DOWNLOAD_DIR})"
+echo "- Delete bisq app dir (${BISQ_APP_DIR})"
+echo "- Delete bisq data dir (${BISQ_DATA_DIR})"
+echo "- Delete bisq start/stop script files in ${HOME_DIR}"
+echo "- Delete .gradle dir in ${HOME_DIR}"
+echo "- Delete .xpra dir in ${HOME_DIR}"
+echo "- Uninstall xpra package via apt-get"
+echo
+echo -e "${LR}Press ${NC}<enter>${LR} key to continue, or ${NC}ctrl-c${LR} to exit${NC}"
+read -r
+
+#-----------------------------------------------------------------
+
 #
 # stop bisq xpra server
 #
 echo
-echo -e "${Y}Stop bisq xpra server...${NC}"
-su -c '${eval ${BISQ_STOP_SCRIPT}' ${USER}
+echo -e "${Y}Stop Bisq xpra server...${NC}"
+su -c ${BISQ_STOP_SCRIPT} ${USER}
 echo -e "${G}Done.${NC}"
 
 #-----------------------------------------------------------------
@@ -41,17 +68,27 @@ echo -e "${G}Done.${NC}"
 # stop bisq gradle server(s) running
 #
 echo
-echo -e "${Y}Stop bisq xpra server...${NC}"
-gradle --stop
+echo -e "${Y}Stop Bisq gradle server...${NC}"
+su -c ${BISQ_APP_DIR}'/./gradlew --stop' ${USER}
 echo -e "${G}Done.${NC}"
 
 #-----------------------------------------------------------------
 
 #
-# delete bisq dir
+# delete bisq download dir
 #
 echo
-echo -e "${Y}Delete bisq app dir...${NC}"
+echo -e "${Y}Delete Bisq download dir (${BISQ_DOWNLOAD_DIR})...${NC}"
+rm -rf "${BISQ_DOWNLOAD_DIR}"
+echo -e "${G}Done.${NC}"
+
+#-----------------------------------------------------------------
+
+#
+# delete bisq app dir
+#
+echo
+echo -e "${Y}Delete Bisq app dir (${BISQ_APP_DIR})...${NC}"
 rm -rf "${BISQ_APP_DIR}"
 echo -e "${G}Done.${NC}"
 
@@ -61,18 +98,8 @@ echo -e "${G}Done.${NC}"
 # delete bisq data dir
 #
 echo
-echo -e "${Y}Delete bisq data dir...${NC}"
+echo -e "${Y}Delete Bisq data dir (${BISQ_DATA_DIR})...${NC}"
 rm -rf "${BISQ_DATA_DIR}"
-echo -e "${G}Done.${NC}"
-
-#-----------------------------------------------------------------
-
-#
-# delete bisq download dir
-#
-echo
-echo -e "${Y}Delete bisq dir...${NC}"
-rm -rf "${BISQ_DOWNLOAD_DIR}"
 echo -e "${G}Done.${NC}"
 
 #-----------------------------------------------------------------
@@ -82,8 +109,8 @@ echo -e "${G}Done.${NC}"
 #
 echo
 echo -e "${Y}Delete bisq start and stop script files...${NC}"
-rm -rf "${BISQ_START_SCRIPT}"
-rm -rf "${BISQ_STOP_SCRIPT}"
+rm -f "${BISQ_START_SCRIPT}"
+rm -f "${BISQ_STOP_SCRIPT}"
 echo -e "${G}Done.${NC}"
 
 #-----------------------------------------------------------------
@@ -115,13 +142,12 @@ echo
 echo -e "${Y}Uninstall xpra via apt...${NC}"
 apt -y purge xpra
 # autoremoving packages and cleaning apt data
-echo -e "${LB}aAutoremoving packages and cleaning apt data...${NC}"
+echo -e "${LB}Autoremoving packages and cleaning apt data...${NC}"
 apt -y autoremove && apt -y clean
 echo -e "${G}Done.${NC}"
 
 #-----------------------------------------------------------------
 
 echo
-echo -e "${Y}Un-installation all done!${NC}"
+echo -e "${Y}Uninstallation all done!${NC}"
 echo
-
